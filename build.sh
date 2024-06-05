0bc9423f315910e7541a02cede5b3b1e4928fb42
Nesswit <rishubil@gmail.com>
2024-06-06 01:06:41 +0900

    Fix font names

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python ./src/svg_to_font.py --svg-dir ./src/inaffont-meconopsis/regular-glyphs --family-name "INAFFont Meconopsis" --font-name "INAFFont Meconopsis" --weight-name "Regular" --output "./dist/inaffont-meconopsis-regular.otf"
-python ./src/svg_to_font.py --svg-dir ./src/inaffont-meconopsis/bold-glyphs --family-name "INAFFont Meconopsis" --font-name "INAFFont Meconopsis" --weight-name "Bold" --output "./dist/inaffont-meconopsis-bold.otf"
+python ./src/svg_to_font.py --svg-dir ./src/inaffont-meconopsis/regular-glyphs --family-name "INAFFont Meconopsis" --font-name "INAFFont Meconopsis Regular" --weight-name "Regular" --version "0.2" --output "./dist/inaffont-meconopsis-regular.otf"
+python ./src/svg_to_font.py --svg-dir ./src/inaffont-meconopsis/bold-glyphs --family-name "INAFFont Meconopsis" --font-name "INAFFont Meconopsis Bold" --weight-name "Bold" --version "0.2" --output "./dist/inaffont-meconopsis-bold.otf"
